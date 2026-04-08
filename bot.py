2255dfd59abb0ca235f90cc250feb756753a8277
anujedits42-byte <anujedits42@gmail.com>
2026-04-08 07:33:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -93,7 +93,7 @@ class Bot(Client):
             f"<b>Bot:</b> @{me.username}\n"
             f"<b>Users:</b> <code>{user_count} / 200</code>\n"
             f"<b>Time:</b> <code>{now.strftime('%I:%M %p')} IST</code>\n\n"
-            f"<b>Developed by @RexBots_Official</b>"
+            f"<b>Developed by @anujedits76</b>"
         )
 
         try:
