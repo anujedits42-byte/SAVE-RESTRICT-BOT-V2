6e9e1cc9031749e4e5c8016562ce6f6b43b9319d
anujedits42-byte <anujedits42@gmail.com>
2026-04-12 20:39:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ import os
 # Telegram Bot Credentials
 # ==============================
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8485375487:AAGbjy5K4PPcOkAlTKDFdYvQe04L_VmLkFw")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8709604452:AAG4zUKUTjhjFPDA_SHmC-IhhUHxTzKHpzA")
 API_ID = int(os.environ.get("API_ID", "34446649"))
 API_HASH = os.environ.get("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
 
@@ -40,7 +40,7 @@ DB_NAME = os.environ.get("DB_NAME", "SaveRestricted2")
 # ==============================
 
 # Replace with your Telegram log channel ID (example: -1001234567890)
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003515041061"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003475522251"))
 
 # ==============================
 # Error Handling
